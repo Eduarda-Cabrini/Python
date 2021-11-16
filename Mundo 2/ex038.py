02ebeb562beb822ede09bac18d0951fdedd79677
Eduarda Luiza Cabrini <dudaluiza2705@gmail.com>
2021-11-16 11:20:15 -0300

    Comparando números

---
+++ Mundo 2/ex038.py
@@ -0,0 +1,8 @@
+p = float(input('Digite o primeiro número:'))
+s = float(input('Digite o segundo número:'))
+if p > s:
+    print('O primeiro valor é maior')
+elif s > p: 
+    print('O segundo valor é maior')  
+elif p == s:
+    print('Os dois são iguais')  
