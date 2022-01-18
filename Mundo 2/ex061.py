62d5c593704be0672f7672d0ff05d567b717688a
Eduarda Luiza Cabrini <dudaluiza2705@gmail.com>
2022-01-18 09:54:28 -0300

    Progressão aritmética 2.0

---
+++ Mundo 2/ex061.py
@@ -0,0 +1,12 @@
+
+print('Gerador de PA')
+print('-=' * 10)
+primeiro = int(input('Primeiro termo: '))
+Razão = int(input('Razão de PA: '))
+termo = primeiro
+cont = 1
+while cont <= 10:
+    print('{} -> '.format(termo), end='')
+    termo += Razão 
+    cont += 1
+print('<FIM>')
