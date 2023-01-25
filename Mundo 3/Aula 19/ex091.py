c36b7307c74ca67869b7a448c7105dbd27788b33
Eduarda Luiza Cabrini <dudaluiza2705@gmail.com>
2023-01-25 10:18:59 -0300

    Jogo com dados

---
+++ Mundo 3/Aula 19/ex091.py
@@ -0,0 +1,24 @@
+
+from random import randint
+from time import sleep 
+from operator import itemgetter
+jogo = {'jogador1:': randint(1, 6),
+        'jogador2:': randint(1, 6),
+        'jogador3:': randint(1, 6),
+        'jogador4:': randint(1, 6)}
+print()
+ranking = {}
+print('Valores sorteados:')
+print()
+for k, v in jogo.items():
+    print(f' {k} tirou {v} no dado!')
+    sleep(2)
+    print()
+ranking = sorted(jogo.items(), key=itemgetter(1), reverse=True)
+print(ranking)
+print( '########### O ranking ficou ############')
+print()
+for i, v in enumerate(ranking):
+    print(f'{i+1}º lugar: {v[0]} com {v[1]}.')
+    sleep(2)
+    print()
